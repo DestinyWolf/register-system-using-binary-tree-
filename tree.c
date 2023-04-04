12843d2cc03e8489b9ffd2f5360d251468e50294
Maike <64764136+DestinyWolf@users.noreply.github.com>
2023-04-04 18:20:04 -0300

    add insert function and search function

--- tree.c
+++ tree.c
@@ -1,5 +1,49 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
 #include "tree.h"
 
+
+
+tree * search(tree * root, int id){
+	if (tree.data.cpf == id){
+		return tree;
+	}
+	else if(tree.data.cpf < id)
+		return search(tree.r, id);
+	else
+		return search(tree.l, id);
+}
+
+
+int insert(tree ** root, typedata data){
+	tree*leaf = (tree*) malloc(sizeof(tree));
+	tree*parent = *root
+	
+	leaf->l = NULL;//aterro 
+	leaf->r = NULL;//aterro o lado esquerdo da folha
+	
+	leaf.data = data;
+	leaf.bal = 0;
+	
+	if(*root){ // se a raiz não for nula 
+		while(parent.r != NULL || parent.l != NULL){ //enquanto não chegar em uma folha o loop repete
+			if (parent.data.cpf > data.cpf)
+				parent = parent.l;
+			else
+				parent = parent.r;
+		}
+		
+		if (parent.data.cpf > data)
+			parent.l = leaf;
+			parent.bal --;
+		
+		else
+			parent.r = leaf;
+			
+		leaf.parent = parent;
+	}
+	else{
+		*root = leaf;
+		leaf.parent = NULL;
+	}
+}
