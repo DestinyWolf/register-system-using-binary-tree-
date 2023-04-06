516e73be070e92d9ab775661c6c842a1dfa80240
DestinyWolf <maikoy606@hotmail.com>
2023-04-06 11:17:00 -0300

    add remove function and any  commits

--- tree.c
+++ tree.c
@@ -27,7 +27,7 @@ int insert(tree ** root, typedata data){
 	
 	if(*root){ // se a raiz não for nula 
 		while(parent->r != NULL || parent->l != NULL){ //enquanto não chegar em uma folha o loop repete
-			if (parent->data.cpf > data.cpf)
+			if (parent->data.cpf > data.cpf) 
 				parent = parent->l;
 			else
 				parent = parent->r;
@@ -47,4 +47,53 @@ int insert(tree ** root, typedata data){
 		*root = leaf;
 		leaf->parent = NULL;
 	}
+}
+
+int remove(tree **root, int id)
+{
+    tree* parent = search((*root), id); //usa a função search pra encontrar o galho/folha que sera removido
+
+	if(parent){ // verifico se o galho que será removido existe
+		if(parent->l == NULL && parent->r == NULL){ //caso ambos os lados sejam nulos
+
+			if(parent->parent->data.cpf > parent->data.cpf){ //verifico se é filho esquerdo do pai
+				parent->parent->l = NULL;// se for atribuu nulo ao filho esquerdo do pai
+				parent->parent->bal ++; //removo do fator de balancento 1
+			}
+			else{//caso contrario
+				parent->parent->r = NULL; //atribuo nulo ao filho direito do pai
+				parent->parent->bal --; //removo um do fator de balanceamento
+			}
+		}
+
+		else if(parent->l != NULL && parent->r == NULL){ //caso ele tenha um filho esquerdo
+			if(parent->parent->data.cpf > parent->data.cpf){ // verifico se ele é o filho esquerdo do pai 
+
+				parent->parent->l = parent->l; //atribuo ao filho esquerdo do avo o filho esquerdo do pai
+				//free
+			}
+			else{ // caso seja o filho direito
+				parent->parent->r = parent->l; //atribuo ao filho direito do avo o filho esquerdo do pai
+				//free
+			}
+		}
+
+		else if(parent->l == NULL && parent->r != NULL){ //caso ele tenha um filho direito
+			if(parent->parent->data.cpf > parent->data.cpf){ // verifico se ele é o filho esquerdo do pai 
+
+				parent->parent->l = parent->r; //atribuo ao filho esquerdo do avo o filho esquerdo do pai
+				//free
+			}
+			else{ // caso seja o filho direito
+				parent->parent->r = parent->r; //atribuo ao filho direito do avo o filho esquerdo do pai
+				//free
+			}
+		}
+
+		else if(parent->l != NULL && parent->r != NULL){} //caso ele tenha dois filhos
+	}
+
+	
+	
+	return 0;
 }
